f2d43f622b14c0cd7b12496c0468fadf0b9e0966
Jordano <jjordano360@gmail.com>
2018-12-07 10:07:09 -0200

    Criação do clsConexaoDAO.php ;)

---
+++ dao/clsConexaoDAO.php
@@ -0,0 +1,53 @@
+<?php
+
+class Conexao {
+    
+    private static function abrir(){
+        $banco = "m171_09_portalweb";
+        $local = "senacinfo-db";
+        $usuario = "inf_m171";
+        $senha = "senacrs";
+        
+        $conn = mysqli_connect($local, $usuario, $senha, $banco);
+        
+        if ( $conn )
+            return $conn;
+        else 
+            return NULL;
+        
+    }
+    
+    private static function fechar( $conn ){
+        mysqli_close( $conn );
+    }
+    
+    public static function executar( $sql ){
+        $conn = self::abrir();
+        if( $conn ){
+            mysqli_query($conn, $sql);
+            self::fechar( $conn );
+        }
+    } 
+        
+    public static function consultar( $sql ){
+        $conn = self::abrir();
+        if( $conn ){
+            $result = mysqli_query($conn, $sql);
+            self::fechar( $conn );
+            return $result;
+        } else {
+            return NULL;
+        }
+    } 
+    
+}
+
+
+
+
+
+
+
+
+
+
